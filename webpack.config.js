7ac0d314e6b88f5024fb7e7060ace833b6df5beb
BrinLewis <brinsley22@hotmail.co.uk>
2023-03-23 10:57:33 +0000

    Build webpack environment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    assetModuleFilename: "[name][ext]",
+    clean: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Odin | Weather App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
