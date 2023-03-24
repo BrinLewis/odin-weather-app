6bcb2b8d4e47ecbb028c75ca63e8c5bd4005a6bf
BrinLewis <brinsley22@hotmail.co.uk>
2023-03-24 14:48:11 +0000

    Create API functions to fetch and process data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
 import "./styles.css";
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/forecast.json?key=222ad05058b944fc8ab123938232303&q=Bristol&days=7&aqi=no&alerts=no"
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch {
+    alert("Something went wrong!");
+  }
+}
+
+async function getRelevantData() {
+  const allData = await fetchData();
+
+  let relevantData = {
+    location: allData.location.name,
+    name: allData.location.country,
+    currentTempC: allData.current.temp_c,
+    currentTempF: allData.current.temp_f,
+    currentDateTime: allData.current.last_updated,
+    condition: allData.current.condition.text,
+    icon: allData.current.condition.icon,
+    feelsLikeC: allData.current.feelslike_c,
+    feelsLikeF: allData.current.feelslike_f,
+    humidity: allData.current.humidity,
+    chanceOfRainToday: allData.forecast.forecastday[0].day.daily_chance_of_rain,
+    windSpeed: allData.current.wind_mph,
+    allWeekForecast: allData.forecast.forecastday,
+    allDayForecast: allData.forecast.forecastday[0].hour,
+  }
+
+  console.log(relevantData);
+  return relevantData;
+}
+
+getRelevantData()
+
+// Data I need
+// .location.name   x
+//          .country   x
+//
+// .current.feelslike_c
+//         .feelslike_f
+//         .wind_kph
+//         .wind_mph
+//         .humidity
+//         .temp_c   x
+//         .temp_f   x
+//
+//         .condition.text
+//                   .icon
+//
+//
+//
+// Getting next 4 hours forecast:
+// Get data.current.last_updated_epoch, loop through each hour of data.forecast.forecastday[0].hour, and if last updated < current time_epoch, add it to "next 4 hours" array and increment i (stop looping when i = 3)
